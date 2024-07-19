81817e6ecf8c9616996951bf49241928b8c60772
Felix A. Schultz <felix.schultz@engapho.com>
2024-07-19 03:22:23 +0200

    Refactor cookie settings logic in gdpr.dev.js

--- uc.js
+++ uc.js
@@ -1299,12 +1299,13 @@ function intaSetCookieSettings() {
     const urlParams = new URLSearchParams(window.location.search);
     const cookieSettings = urlParams.get('intaCookieSettings');
 
+    console.log("Cookie Settings from URL", cookieSettings);
     if (cookieSettings) {
         console.log("Setting the new cookie", cookieLifeTime);
         document.cookie = int_hideCookieBannerName + "=" + cookieSettings + "; expires=" + cookieLifeTime +
             "; path=/; " +
             intCookieDomain +
-            "";
+            " SameSite=None; Secure";
     }
 };
 
