9dd921ae35004c51a5a9c30c9fe90c62f96f6a5e
Felix A. Schultz <felix.schultz@engapho.com>
2024-07-19 03:45:31 +0200

    Refactor cookie settings logic in uc.js and gdpr.dev.js

--- uc.js
+++ uc.js
@@ -183,8 +183,26 @@ tmpl.innerHTML = `
 <slot></slot>
 `;
 
-window.addEventListener("DOMContentLoaded", (event) => {
+/* - - - Function to get Cookie Settings from url and set the cookie - - - */
+function intaSetCookieSettings() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const cookieSettings = urlParams.get('intaCookieSettings');
+
+    console.log("Cookie Settings from URL", window.location.host);
+    if (cookieSettings) {
+        console.log("Setting the new cookie");
+        document.cookie = int_hideCookieBannerName + "=" + cookieSettings + "; expires=" + cookieLifeTime +
+            "; path=/; " +
+            intCookieDomain +
+            "SameSite=Lax";
+    }
+};
+
+window.addEventListener("load", (event) => {
     intaSetCookieSettings();
+});
+
+window.addEventListener("DOMContentLoaded", (event) => {
     customElements.define('inta-consents-content', class extends HTMLElement {
         constructor() {
             super(); // always call super() first in the constructor.
@@ -1293,22 +1311,6 @@ if (typeof window.INTA.policy_link === "undefined" && document.querySelector('sc
 
     document.head.insertBefore(configScript, document.currentScript);
 }
-
-/* - - - Function to get Cookie Settings from url and set the cookie - - - */
-function intaSetCookieSettings() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const cookieSettings = urlParams.get('intaCookieSettings');
-
-    console.log("Cookie Settings from URL", window.location.host);
-    if (cookieSettings) {
-        console.log("Setting the new cookie");
-        document.cookie = int_hideCookieBannerName + "=" + cookieSettings + "; expires=" + cookieLifeTime +
-            "; path=/; " +
-            intCookieDomain +
-            "SameSite=Lax";
-    }
-};
-
 /* - - - Helper function to get cookie type*/
 function intaCookieType(type) {
     if (getCookie(type) === "checked") return true;
