d9167759e71622956d05a2d324f49165e9d20117
Felix A. Schultz <felix.schultz@engapho.com>
2024-08-15 18:39:06 +0200

    Refactor cookie banner code to use encoded intastellar user consents

--- dev/cb.dev.js
+++ dev/cb.dev.js
@@ -772,7 +772,7 @@ window.addEventListener("load", function () {
             if (event.data.cookieSharing !== undefined || event.data.cookieSharing !== null) {
                 const intastellarUserGivingConsents = event.data.cookieSharing;
                 document.cookie =
-                    int_hideCookieBannerName + "=__inta1." + encodeIntaConsentsObject(JSON.stringify(intastellarUserGivingConsents), randomIntFromInterval(20, 34)) + "; expires=" + cookieLifeTime +
+                    int_hideCookieBannerName + "=__inta1." + encodeIntaConsentsObject(intastellarUserGivingConsents, randomIntFromInterval(20, 34)) + "; expires=" + cookieLifeTime +
                     "; path=/; " +
                     intCookieDomain +
                     "";
