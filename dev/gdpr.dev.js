37ecdc402bc62b330b59a51cf2bd87c30e348800
Felix A. Schultz <felix.schultz@engapho.com>
2024-10-28 16:55:32 +0100

    Update warning message to specify WordPress cookie solutions version

--- dev/gdpr.dev.js
+++ dev/gdpr.dev.js
@@ -221,7 +221,7 @@ function intaSetCookieSettings() {
 
 window.addEventListener("DOMContentLoaded", (event) => {
     if (document.getElementById("intastellar-gdpr-settings-js-after") !== null) {
-        console.warn(`IntastellarSolutionsSDK: You´re using an old version of our cookie solutions, to make sure to get the latest updates and features, please download the latest version from our Website:
+        console.warn(`IntastellarSolutionsSDK: You´re using an old version of our cookie solutions for WordPress. To make sure to get the latest updates and features, please download the latest version from our Website:
         https://www.intastellarsolutions.com/cookie-solutions/downloads`);
     }
     customElements.define('inta-consents-content', class extends HTMLElement {
