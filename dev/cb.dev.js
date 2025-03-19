79f66310ce06e86cf29289a771e7ecbebd1d607e
Felix A. Schultz <felix.schultz@engapho.com>
2025-03-19 15:29:08 +0100

    Setting a small timeout (#223)
    * Update consent handling for Facebook and Google ads
    
    * Update README.md (#199)
    
    Signed-off-by: Felix A. Schultz <felix.schultz@engapho.com>
    
    * Add clarity consent handling and update fbq consent revocation
    
    * Add dataLayer event for cookie consent widget visibility
    
    * Add dataLayer event for cookie consent widget visibility
    
    * Moved the loading of the stylesheet to the cb file
    
    * Update gdpr.dev.js
    
    Sending default consent banner loaded script
    
    Signed-off-by: Felix A. Schultz <felix.schultz@engapho.com>
    
    * Update gdpr.dev.js (#204)
    
    Added consents banner loaded event
    
    Signed-off-by: Felix A. Schultz <felix.schultz@engapho.com>
    
    * Update uc.js (#205)
    
    Added google tag loading check.
    
    Signed-off-by: Felix A. Schultz <felix.schultz@engapho.com>
    
    * Update uc.js (#207)
    
    Signed-off-by: Felix A. Schultz <felix.schultz@engapho.com>
    
    * Remove unused dataLayer push and simplify gtag consent banner loading
    
    * Add HubSpot cookie consent preference cookie to requiredCookieList
    
    * Fix language detection for intastellarCookieLanguage and set default to English
    
    * Enhance language detection for cookie settings by incorporating window.INTA.settings.language
    
    * Refactor language detection logic for cookie settings to improve accuracy
    
    * Remove unused dataLayer push and refine cookie consent condition for clarity
    
    * Remove unnecessary type attribute from gtag.js script tag for cleaner HTML
    
    * Enhance cookie consent logic by adding checkbox checks for functional, static, and marketing cookies
    
    * Add cookie status check after updating consent settings
    
    * Replace cookie status check with page reload after updating consent settings
    
    * Remove unnecessary console log from checkCookieStatus function
    
    * Update consent banner event tracking to use dataLayer
    
    * Remove redundant page reload in saveINTCookieSettings function
    
    * Enhance cookie consent logic to include checkbox states for functional, statistic, and marketing cookies
    
    * Implement consent tracking for cookie settings and enhance script processing based on user consent
    
    * Add restartObserver call to saveINTCookieSettings function
    
    * Add delay to restartObserver call in saveINTCookieSettings function
    
    ---------
    
    Signed-off-by: Felix A. Schultz <felix.schultz@engapho.com>

--- dev/cb.dev.js
+++ dev/cb.dev.js
@@ -3821,7 +3821,9 @@ function saveINTCookieSettings(consent, type = null) {
         "";
     /*window.location.reload();*/
     updateConsents(consent, type);
-    restartObserver();
+    setTimeout(() => {
+        restartObserver();
+    }, 500);
     document.querySelector("[name=intastellar-solutions-sharinglibrary-iframe]").contentWindow
         .postMessage(JSON.stringify(intaConsentsObjectVariable), "*");
 }
