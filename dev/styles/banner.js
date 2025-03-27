2bfac5a078a71c839e256d7aaf9b2f9a0762f2b6
Felix A. Schultz <fs@asasoftware.aero>
2025-03-27 13:18:32 +0100

    Refactor banner.js and overlay.js: Update class assignments and comment out unused code

--- dev/styles/banner.js
+++ dev/styles/banner.js
@@ -55,7 +55,7 @@ intaconsentsContainer.appendChild(cookieSettings);
 
 cookieSettings.appendChild(cookieSettingsContent);
 intastellarConsentsBannerContent.appendChild(intaconsentsContainer);
-intastellarConsentsBannerContent.appendChild(poweredBy);
+/* intastellarConsentsBannerContent.appendChild(poweredBy); */
 intastellarConsentsBannerContent.appendChild(cookieSettings);
 
 intastellarConsentsBanner.appendChild(intastellarConsentsBannerContent);
--- dev/styles/overlay.js
+++ dev/styles/overlay.js
@@ -15,8 +15,8 @@ const testSection = document.createElement("section");
 const cookieSettings = document.createElement("article");
 const cookieSettingsContent = document.createElement("section");
 
-moreSettings.setAttribute("class", "intastellarCookieConstents");
-moreSettingsContent.setAttribute("class", "intastellarCookieConstents__content");
+intastellarConsentsBannerContent.setAttribute("class", "intastellarCookieConstents");
+intastellarConsentsBannerContentContent.setAttribute("class", "intastellarCookieConstents__content");
 moreintHeader.setAttribute("class", "intastellarCookieConstents__content-intHeader");
 moreFooter.setAttribute("class", "intastellarCookieConstents__content-footer");
 
@@ -27,7 +27,7 @@ testSection.appendChild(moreintHeader);
 
 testSection.appendChild(moreContentText);
 
-moreSettingsContent.appendChild(intastellarCookieConstents__Container);
+intastellarConsentsBannerContentContent.appendChild(intastellarCookieConstents__Container);
 intastellarCookieConstents__Container.appendChild(testSection);
 intastellarCookieConstents__Container.appendChild(intastellarCookieButtons);
 intastellarCookieConstents__Container.appendChild(moreFooter);
@@ -53,4 +53,7 @@ intaconsentsContainer.appendChild(intastellarSettingsButton);
 intaconsentsContainer.appendChild(cookieSettings);
 
 cookieSettings.appendChild(cookieSettingsContent);
-cookieSettingsContent.appendChild(poweredBy);
+/* cookieSettingsContent.appendChild(poweredBy); */
+intastellarConsentsBannerContent.appendChild(cookieSettings);
+
+intastellarConsentsBanner.appendChild(intastellarConsentsBannerContent);
