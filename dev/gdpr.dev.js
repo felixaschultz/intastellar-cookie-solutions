9618f13f239af2ebf9b29b35df1049c13bbfb873
Felix A. Schultz <fs@asasoftware.aero>
2025-03-31 09:14:51 +0200

    Add HubSpot cookie definitions and privacy policy links to GDPR script

--- dev/gdpr.dev.js
+++ dev/gdpr.dev.js
@@ -656,6 +656,31 @@ const inta_requiredCookieList = [{
     domains: [
         window.location.host
     ]
+},
+{
+    vendor: "HubSpot",
+    cookies: [
+        {
+            cookie: "__hs_opt_out",
+            purpose: "This cookie is used by the opt-in privacy policy to remember not to ask the visitor to accept cookies again.",
+        },
+        {
+            cookie: "__hs_do_not_track",
+            purpose: "This cookie can be set to prevent the HubSpot tracking cookie from being set.",
+        },
+        {
+            cookie: "__hs_initial_opt_in",
+            purpose: "This cookie is used to prevent the banner from always displaying when visitors are browsing in strict mode.",
+        },
+        {
+            cookie: "__hs_cookie_cat_prefs",
+            purpose: "This cookie is used to store the HubSpot cookie category preferences of a visitor.",
+        }
+    ],
+    vendor_privacy: "https://legal.hubspot.com/privacy-policy",
+    domains: [
+        window.location.host
+    ]
 }
 ];
 /* - - - List of Analytics / Statistics cookie names - - - */
