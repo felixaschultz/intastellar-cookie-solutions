fca861c07b23d2e8bdcafa1957137bb10751b2be
Felix A. Schultz <felix.schultz@engapho.com>
2025-04-22 18:06:03 +0200

    Refactor banner.js: Conditionally append moreintHeader based on window width and comment out poweredBy logic

--- dev/styles/banner.js
+++ dev/styles/banner.js
@@ -9,7 +9,10 @@ moreContentText.setAttribute("class", "intastellarCookieConstents__content-main"
 moreSettingsContent.setAttribute("class", "intastellarCookieConstents__content");
 
 testSection.setAttribute("class", "intastellarCookieConstents__contentC");
-testSection.appendChild(moreintHeader);
+
+if (window.innerWidth <= 768) {
+    testSection.appendChild(moreintHeader);
+}
 
 testSection.appendChild(moreContentText);
 
@@ -27,9 +30,9 @@ cookieSettings.setAttribute("class", "intastellarCookie-settings__container");
 intastellarSettingsButton.setAttribute("onclick", "javascript:IntastellarCookieConsent.renew();");
 intastellarSettingsButtonContent.innerHTML = '<img class="intCookieIcon-openSettings" style="filter: brightness(' + (darkLightCheck(window.INTA.settings.color) === "light" ? "0" : "100") + ') !important" src="' + intCookieIcon + '" alt="Cookie Icon">' + IntastellarToolTip + ' ' + text;
 
-if (window.location.host.indexOf("intastellarsolutions") == -1) {
+/* if (window.location.host.indexOf("intastellarsolutions") == -1) {
     poweredBy = "<span class='intastellarCookie-settings__poweredBy' alt='This cookie banner is powered by Intastellar Consents Solutions'>Powered by <a class='intastellarCookie-settings__poweredByLink' href='https://www.intastellarsolutions.com?utm_source=" + encodeURI(window.location.href) + "&utm_content=powered_by&utm_medium=referral&utm_campaign=" + pluginSource + "&utm_term=gdpr_banner_logo' target='_blank' rel='noopener'><img class='intastellarCookie-settings__poweredByImg' width='100px' height='100px' src='" + intastellarLogo + "' alt='Intastellar Solutions, International'></a></span>";
-}
+} */
 if (arrange == "ltr") {
     intastellarSettingsButton.classList.add("intastellarCookie-settingsContainer--otherSide");
     cookieSettings.classList.add("intastellarCookie-settings__container--otherSide");
@@ -40,6 +43,7 @@ let CompanyLogoName = cookieLogo == intCookieIcon ? "Cookie Icon" : `${document.
 
 moreintHeader.innerHTML = `
     ${typeof window?.INTA?.settings.logo != "undefined" ? '<img class="intSettingsCompanyLogo" src="' + window?.INTA?.settings.logo + '" alt="' + CompanyLogoName + '" title="' + CompanyLogoName + '">' : ``}
+    ${(window.INTA.settings.design == "banner" && window.innerWidth <= 768 ? generatePoweredBy() : "")}
     `;
 
 moreContentText.innerHTML = settingsMessage;
