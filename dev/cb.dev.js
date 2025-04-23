6b193004c74e9432522acdafa79dbf62e0307167
Felix A. Schultz <fs@asasoftware.aero>
2025-04-23 13:49:22 +0200

    Refactor cookie consent initialization to improve loading logic
    - Changed the initialization check from 'loading' to 'complete' for better performance.
    - Removed unnecessary event listener for 'DOMContentLoaded'.
    - Streamlined the script loading process for the cookie consent configuration.
    - Ensured the template is appended to the body after the script loading logic.

--- dev/cb.dev.js
+++ dev/cb.dev.js
@@ -3219,57 +3219,54 @@ const IntastellarCookieConsent = {
         template.classList.remove("--active");
     },
     initialize: function (template) {
+        if (document.readyState === 'complete') {
+            if (
+                document.querySelectorAll('script[src^="https://downloads.intastellarsolutions.com/cookieconsents/"][src$="/config.js"]').length === 0
+                || window.INTA === undefined
+            ) {
+                // Get the host and remove all subdomains
+                let host = window.location.host;
+                host.replace(/^(?:https?:\/\/)?(?:www\.)?/i, "");
+                // Remove the port if it exists
+                host = host.replace(/:\d+$/, "");
+                const intastellarDefaultConfigFile = "https://downloads.intastellarsolutions.com/cookieconsents/" + host + "/config.js";
+                const configScript = document.createElement("script");
+                configScript.src = intastellarDefaultConfigFile;
 
-        if (document.readyState === "loading") {
-            document.addEventListener('DOMContentLoaded', () => {
-                if (
-                    document.querySelectorAll('script[src^="https://downloads.intastellarsolutions.com/cookieconsents/"][src$="/config.js"]').length === 0
-                    || window.INTA === undefined
-                ) {
-                    // Get the host and remove all subdomains
-                    let host = window.location.host;
-                    host.replace(/^(?:https?:\/\/)?(?:www\.)?/i, "");
-                    // Remove the port if it exists
-                    host = host.replace(/:\d+$/, "");
-                    const intastellarDefaultConfigFile = "https://downloads.intastellarsolutions.com/cookieconsents/" + host + "/config.js";
-                    const configScript = document.createElement("script");
-                    configScript.src = intastellarDefaultConfigFile;
+                const xhr = new XMLHttpRequest();
+                xhr.open("GET", intastellarDefaultConfigFile);
+                xhr.send();
 
-                    const xhr = new XMLHttpRequest();
-                    xhr.open("GET", intastellarDefaultConfigFile);
-                    xhr.send();
-
-                    if (xhr.status === 200) {
-                        document.head.insertBefore(configScript, document.currentScript);
-                    }
+                if (xhr.status === 200) {
+                    document.head.insertBefore(configScript, document.currentScript);
                 }
-                document.body.append(template);
-            });
-        } else {
-            if (document.readyState === 'complete') {
-                if (
-                    document.querySelectorAll('script[src^="https://downloads.intastellarsolutions.com/cookieconsents/"][src$="/config.js"]').length === 0
-                    || window.INTA === undefined
-                ) {
-                    // Get the host and remove all subdomains
-                    let host = window.location.host;
-                    host.replace(/^(?:https?:\/\/)?(?:www\.)?/i, "");
-                    // Remove the port if it exists
-                    host = host.replace(/:\d+$/, "");
-                    const intastellarDefaultConfigFile = "https://downloads.intastellarsolutions.com/cookieconsents/" + host + "/config.js";
-                    const configScript = document.createElement("script");
-                    configScript.src = intastellarDefaultConfigFile;
-
-                    const xhr = new XMLHttpRequest();
-                    xhr.open("GET", intastellarDefaultConfigFile);
-                    xhr.send();
-
-                    if (xhr.status === 200) {
-                        document.head.insertBefore(configScript, document.currentScript);
-                    }
-                }
-                document.body.append(template);
             }
+            document.body.append(template);
+        } else {
+            window.addEventListener("load", function () {
+                if (
+                    document.querySelectorAll('script[src^="https://downloads.intastellarsolutions.com/cookieconsents/"][src$="/config.js"]').length === 0
+                    || window.INTA === undefined
+                ) {
+                    // Get the host and remove all subdomains
+                    let host = window.location.host;
+                    host.replace(/^(?:https?:\/\/)?(?:www\.)?/i, "");
+                    // Remove the port if it exists
+                    host = host.replace(/:\d+$/, "");
+                    const intastellarDefaultConfigFile = "https://downloads.intastellarsolutions.com/cookieconsents/" + host + "/config.js";
+                    const configScript = document.createElement("script");
+                    configScript.src = intastellarDefaultConfigFile;
+
+                    const xhr = new XMLHttpRequest();
+                    xhr.open("GET", intastellarDefaultConfigFile);
+                    xhr.send();
+
+                    if (xhr.status === 200) {
+                        document.head.insertBefore(configScript, document.currentScript);
+                    }
+                }
+                document.body.append(template);
+            })
         }
     }
 }
