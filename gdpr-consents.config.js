4f118701782f87b6fa58f814566dbf256da4a9ef
Felix A. Schultz <felix.schultz@engapho.com>
2025-07-05 23:45:11 +0200

    Update consent configuration and language settings; enhance script filtering

--- gdpr-consents.config.js
+++ gdpr-consents.config.js
@@ -6,11 +6,11 @@ window.INTA = {
     settings: {
         company: "Test Company",
         logo: "https://www.intastellarsolutions.com/assets/logos/intastellar-new-planet.svg",
-        /* color: "radial-gradient(circle,rgba(226, 0, 15, 1) 0%,rgba(182, 0, 15, 1) 100%)", */
-        color: "#c33333",
+        color: "radial-gradient(circle,rgba(226, 0, 15, 1) 0%,rgba(182, 0, 15, 1) 100%)",
+        /* color: "#c33333", */
         /* color: "#e2000f", */
         text: false,
-        language: "arabic",
+        language: "auto",
         design: "bannerV2",
         requiredCookies: [
             {
