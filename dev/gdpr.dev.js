7498246bb4a3cf0ada94bdb6c5989709af2602cd
Felix A. Schultz <fs@asasoftware.aero>
2025-09-16 10:22:05 +0200

    Fixing intastellarDevMode

--- dev/gdpr.dev.js
+++ dev/gdpr.dev.js
@@ -262,11 +262,10 @@ function decodeIntaConsentsObject(number) {
 
 const intastellarDevMode = (function () {
     return window.location.host === "localhost"
-        || window.location.host.indexOf("127.0.0.1") > -1
-        || window.location.host.indexOf("0.0.0.0") > -1
-        || window.location.host.indexOf("192.168.") > -1
-        || window.location.host.indexOf("::1") > -1
-        && window.INTA.dev === true
+        || window.location.host.indexOf("127.0.0.1") > -1 && window.INTA.dev === true
+        || window.location.host.indexOf("0.0.0.0") > -1 && window.INTA.dev === true
+        || window.location.host.indexOf("192.168.") > -1 && window.INTA.dev === true
+        || window.location.host.indexOf("::1") > -1 && window.INTA.dev === true
         ? true : false;
 })();
 
