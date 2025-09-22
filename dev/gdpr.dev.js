b4d2c207db0dcb91176f0f90e80d5a251e934f2c
Felix A. Schultz <fs@asasoftware.aero>
2025-09-22 12:15:52 +0200

    Fixing consent defaults

--- dev/gdpr.dev.js
+++ dev/gdpr.dev.js
@@ -99,29 +99,6 @@ if (!isGtmMode && !window._gtagDefaultFired && typeof gtag === 'function') {
         });
         console.log("Intastellar Consents: Applied REST-OF-WORLD defaults");
         window._gtagDefaultFired = true;
-
-        if (typeof gtag === 'function') {
-            gtag('get', 'ad_storage', 'default', (value) => {
-                console.log('ad_storage default:', value);
-            });
-
-            gtag('get', 'analytics_storage', 'default', (value) => {
-                console.log('analytics_storage default:', value);
-            });
-
-            gtag('get', 'personalization_storage', 'default', (value) => {
-                console.log('personalization_storage default:', value);
-            });
-
-            gtag('get', 'functionality_storage', 'default', (value) => {
-                console.log('functionality_storage default:', value);
-            });
-
-            gtag('get', 'security_storage', 'default', (value) => {
-                console.log('security_storage default:', value);
-            });
-
-        }
     }
 } else if (isGtmMode) {
     console.log('GTM mode detected - skipping consent default initialization');
