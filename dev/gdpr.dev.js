eba05ec80dd6451e1724764e20248178913a9072
Felix A. Schultz <fs@asasoftware.aero>
2025-09-22 14:01:50 +0200

    Reverted to the previouse state

--- dev/gdpr.dev.js
+++ dev/gdpr.dev.js
@@ -108,6 +108,8 @@ if (typeof fbq === "undefined" || typeof fbq === "null") {
     function fbq() { }
 }
 
+window.clarity = window.clarity || function () { (window.clarity.q = window.clarity.q || []).push(arguments) };
+
 window.clarity && window.clarity('consentV2', {
     ad_Storage: "denied",
     analytics_Storage: "denied"
