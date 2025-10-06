74182901323374fd9b3bd7ec90d567e89989f0db
Felix A. Schultz <felix.schultz@engapho.com>
2025-10-06 11:31:17 +0200

    Waiting 200 ms for the banner to be appended to the body

--- cb.js
+++ cb.js
@@ -2391,6 +2391,10 @@ moreSettings.appendChild(moreSettingsContent);
 intaconsents.appendChild(banner);
 intaconsents.appendChild(moreSettings);
 
+setTimeout(()=> {
+    document.body.appendChild(intaconsents);
+}, 200)
+
 if (document.querySelector(".intastellarCCPAContainer") != null) {
     document.querySelector(".intastellarCCPAContainer").addEventListener("click", function () {
         document.querySelector(".intastellarCCPApopup").classList.toggle("--active");
@@ -2403,7 +2407,6 @@ function onWindowLoad(callback) {
 }
 
 onWindowLoad(function () {
-    document.body.appendChild(intaconsents);
     (adsbygoogle = window.adsbygoogle || []).pauseAdRequests = 0;
     const temp = location.host.split('.').reverse();
     const domain = encodeURI(temp[1] + '.' + temp[0]);
