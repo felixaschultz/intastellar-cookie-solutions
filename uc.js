fcfce73b3cd3aba5264b686ebe4d3da879316d95
Felix A. Schultz <felix.schultz@engapho.com>
2025-11-24 22:07:45 +0100

    Updating check

--- uc.js
+++ uc.js
@@ -162,7 +162,7 @@ window.Shopify ?? window?.Shopify?.loadFeatures(
             return;
         }
         // If error is false, the API has loaded and ready to use!
-        window.Shopify.customerPrivacy.setTrackingConsent(
+        window.Shopify && window.Shopify.customerPrivacy.setTrackingConsent(
             {
                 'analytics': intaCookieConsents?.staticsticCookies === "checked",
                 'marketing': intaCookieConsents?.advertisementCookies === "checked",
@@ -2356,7 +2356,7 @@ if (intaCookieConsents?.advertisementCookies) {
         analytics_Storage: "denied"
     });
 
-    window.Shopify ?? window.Shopify.customerPrivacy.setTrackingConsent(
+    window.Shopify && window.Shopify.customerPrivacy.setTrackingConsent(
         {
             'analytics': false,
             'marketing': true,
@@ -2385,7 +2385,7 @@ if (intaCookieConsents?.staticsticCookies) {
         'analytics_storage': 'granted'
     });
 
-    window.Shopify ?? window.Shopify.customerPrivacy.setTrackingConsent(
+    window.Shopify && window.Shopify.customerPrivacy.setTrackingConsent(
         {
             'analytics': true,
             'marketing': false,
@@ -2403,7 +2403,7 @@ if (intaCookieConsents?.functionalCookies) {
         'functionality_storage': 'granted'
     });
 
-    window.Shopify ?? window.Shopify.customerPrivacy.setTrackingConsent(
+    window.Shopify && window.Shopify.customerPrivacy.setTrackingConsent(
         {
             'analytics': false,
             'marketing': false,
