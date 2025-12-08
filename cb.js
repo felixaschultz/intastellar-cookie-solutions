1927a85d7d0e3dbed4ae4c9e617b1cc54c6572f3
Felix A. Schultz <felix.schultz@engapho.com>
2025-12-08 17:03:37 +0100

    Removed window.platform

--- cb.js
+++ cb.js
@@ -92,7 +92,6 @@ const moreintHeader = document.createElement("intheader");
 const moreContentText = document.createElement("section");
 const moreFooter = document.createElement("div");
 const intaconsents = window.intaconsents = document.createElement("intastellarconsents");
-window.platform = findScriptParameter("utm_source") === undefined ? "Manual" : findScriptParameter("utm_source");
 
 const IntastellarCookieConsent = {
     renew: function () {
