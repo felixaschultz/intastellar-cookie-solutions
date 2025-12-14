45ca1584d6d2eed02e258b2ed7423b4060d3e239
Felix A. Schultz <felix.schultz@engapho.com>
2025-12-14 11:32:43 +0100

    Fixed a bug with the domains

--- uc.js
+++ uc.js
@@ -459,7 +459,7 @@ const ALLOWLIST = [
         const parts = host.split('.');
         const subdomains = [];
         for (let i = 0; i < parts.length - 1; i++) {
-            subdomains.push('https://' + parts.slice(i).join('.'));
+            subdomains.push(parts.slice(i).join('.'));
         }
         return subdomains;
     })(),
