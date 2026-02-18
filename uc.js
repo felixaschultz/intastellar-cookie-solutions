e468fb42d95e0a1481d4cc0a0cb41fc7328a8b91
Felix A. Schultz <felix.schultz@engapho.com>
2026-02-18 14:00:42 +0100

    fix: Add new API endpoint to allowlist for enhanced integration with Intastellar accounts

--- uc.js
+++ uc.js
@@ -1334,6 +1334,7 @@ const ALLOWLIST = [
     "https://analytics.intastellarsolutions.com",
     "https://api.intastellarsolutions.com",
     "https://apis.intastellarsolutions.com",
+    "https://apis.intastellaraccounts.com",
     "https://consents.intastellarsolutions.com",
     "https://vendor-list.consensu.org",
     "/dev/gvl-local.json",
